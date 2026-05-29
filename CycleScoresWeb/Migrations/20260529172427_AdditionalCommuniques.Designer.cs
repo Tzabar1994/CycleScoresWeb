cd571bbab6e38e3b1aac5603ff77593110588667
Dan Tzabar <d.tzabar@codat.io>
2026-05-29 18:40:57 +0100

    Add additional communiques

--- CycleScoresWeb/Migrations/20260529172427_AdditionalCommuniques.Designer.cs
+++ CycleScoresWeb/Migrations/20260529172427_AdditionalCommuniques.Designer.cs
@@ -4,6 +4,7 @@ using CycleScoresWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CycleScoresWeb.Migrations
 {
     [DbContext(typeof(CycleScoresWebContext))]
-    partial class CycleScoresWebContextModelSnapshot : ModelSnapshot
+    [Migration("20260529172427_AdditionalCommuniques")]
+    partial class AdditionalCommuniques
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
